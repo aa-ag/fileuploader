4c651c4bc3eb2c5046527cc14f9a5c034d78a970
aa-ag <aaron.aguerrevere@gmail.com>
2020-10-11 16:16:24 -0500

    debu=True

--- app.py
+++ app.py
@@ -44,3 +44,6 @@ def upload_file():
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
